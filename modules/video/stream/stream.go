eeb5b9cd950e771b48c75960a867cf0b25df79cc
LingSung <tumi9325@gmail.com>
2022-06-05 11:22:43 +0800

    fix: revise occurrence spelling

--- modules/video/stream/stream.go
+++ modules/video/stream/stream.go
@@ -38,7 +38,7 @@ func (s *stream) HandleVideoCreated(ctx context.Context, req *pb.HandleVideoCrea
 		variant := strconv.Itoa(int(req.GetScale()))
 
 		// [Kafka TODO]
-		// [Describe] Transcode video if get message with scale != 0, you can handle error occurance like above primitive.ObjectIDFromHex(req.GetId()).
+		// [Describe] Transcode video if get message with scale != 0, you can handle error occurrence like above primitive.ObjectIDFromHex(req.GetId()).
 		if err := s.handleVideoWithVariant(ctx, id, variant, req.GetUrl()); err != nil {
 			return nil, &saramakit.HandlerError{Retry: true, Err: err}
 		}
@@ -47,7 +47,7 @@ func (s *stream) HandleVideoCreated(ctx context.Context, req *pb.HandleVideoCrea
 	}
 
 	// [Kafka TODO]
-	// [Describe] Fanout create events to each variant [1080, 720, 480, 320], you can handle error occurance like above primitive.ObjectIDFromHex(req.GetId()).
+	// [Describe] Fanout create events to each variant [1080, 720, 480, 320], you can handle error occurrence like above primitive.ObjectIDFromHex(req.GetId()).
 	variants := []int32{1080, 720, 480, 320}
 	for _, variant := range variants {
 		if err := s.produceVideoCreatedWithScaleEvent(&pb.HandleVideoCreatedRequest{
